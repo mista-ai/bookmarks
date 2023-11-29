a822d70a3369c69d587731b52079daee8ad12c7b
mista <mr.bezdar@gmail.com>
2023-11-29 07:56:49 +0300

    unqieue email and login by email

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -130,3 +130,8 @@ EMAIL_USE_TLS = True
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
