d5f3df742b65fac3809ec1280828ba847e51564a
mista <mr.bezdar@gmail.com>
2023-11-29 10:32:54 +0300

    social auth done

--- account/authentication.py
+++ account/authentication.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
-
+from account.models import Profile
 
 class EmailAuthBackend:
     """
     Аутентифицировать посредством адреса электронной почты.
     """
+
     def authenticate(self, request, username=None, password=None):
         try:
             user = User.objects.get(email=username)
@@ -19,3 +20,10 @@ class EmailAuthBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
+
+def create_profile(backend, user, *args, **kwargs):
+    """
+    Создать профиль пользователя для социальной аутентификации
+    """
+    Profile.objects.get_or_create(user=user)
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xdovy^tc@qgdlzc1j@v&y28f@nf%035_$ej#u-86$5lc6#q-_v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -134,4 +136,30 @@ MEDIA_ROOT = BASE_DIR / 'media'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.twitter.TwitterOAuth',
+    'social_core.backends.google.GoogleOAuth2',
+]
+
+SOCIAL_AUTH_FACEBOOK_KEY = personal_data.facebook_app_id
+SOCIAL_AUTH_FACEBOOK_SECRET = personal_data.facebook_app_secret
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+
+SOCIAL_AUTH_TWITTER_KEY = personal_data.twitter_api_key  # Ключ API Twitter
+SOCIAL_AUTH_TWITTER_SECRET = personal_data.twitter_api_secret  # Секрет API Twitter
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = personal_data.google_client_id  # ИД клиента Google
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = personal_data.google_client_secret  # Секрет клиента Google
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
 ]
