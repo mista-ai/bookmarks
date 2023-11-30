f6129a52f2cae6ffee9b1c2eb679590cea770856
mista <mr.bezdar@gmail.com>
2023-11-30 10:05:16 +0300

    subscribe system developed

--- account/urls.py
+++ account/urls.py
@@ -3,16 +3,17 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    # предыдущий url входа
+    # # previous login view
     # path('login/', views.user_login, name='login'),
-    # url-адреса входа и выхода
+    #
     # path('login/', auth_views.LoginView.as_view(), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # # url-адреса смены пароля
+    #
+    # # change password urls
     # path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     # path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     #
-    # # url-адреса сброса пароля
+    # # reset password urls
     # path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     # path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
@@ -23,4 +24,7 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
     path('edit/', views.edit, name='edit'),
+    path('users/', views.user_list, name='user_list'),
+    path('users/follow/', views.user_follow, name='user_follow'),
+    path('users/<username>/', views.user_detail, name='user_detail'),
 ]
